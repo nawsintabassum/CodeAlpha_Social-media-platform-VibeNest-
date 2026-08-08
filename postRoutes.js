05fa516a265d57228d183bed42709a343a1c26ab
Nawsin Tabassum <nawsin2006@gmail.com>
2026-08-08 12:36:24 +0600

    postroutes

---
+++ postRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const postController = require('../controllers/postController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+
+router.get('/', postController.getAllPosts);
+
+
+router.post('/', authMiddleware, postController.createPost);
+
+module.exports = router;
+
+ 
